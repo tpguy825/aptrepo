deb5e37d22b8709515e2c0b1c6755f724d41ebbb
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2025-08-24 14:24:02 +0100

    Add git to fetch.sh

--- fetcher/fetch.sh
+++ fetcher/fetch.sh
@@ -2,13 +2,17 @@
 while read -u 10 p; do
 	echo $p
 	bun index.ts $p
-	
+
 	cd ../apt-repo
 
 	dpkg-scanpackages --arch amd64 pool/ > dists/stable/main/binary-amd64/Packages
 	cat dists/stable/main/binary-amd64/Packages | gzip -9 > dists/stable/main/binary-amd64/Packages.gz
 
 	../create-release.sh > dists/stable/Release
+
+	git add .
+	git commit -m "automated: update $p"
+	git push
 
 	cd ../fetcher
 done 10< repos.txt
