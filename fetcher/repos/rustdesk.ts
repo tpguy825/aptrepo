57dde0e1ecd9ecc7beccc1ea41aae7629ba2c0db
Toby Payne <git@tpguy825.uk>
2026-08-21 14:39:52 +0100

    Update rustdesk.ts

--- fetcher/repos/rustdesk.ts
+++ fetcher/repos/rustdesk.ts
@@ -1,3 +1,8 @@
+import { defaultEachFile } from "../index.ts";
 export default {
 	repo: "rustdesk/rustdesk",
+	eachFile(name, contents, release) {
+		if (name.includes("sciter")) return false;
+		return defaultEachFile(name, contents);
+	},
 } satisfies RepoConfig;
